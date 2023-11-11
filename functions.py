95654278baa5689bd650d145df96820ac7010156
will <zhenlanwang0426@gmail.com>
2023-11-11 17:57:58 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
 from torch.utils.data import Dataset, DataLoader
-import nlpaug.augmenter.word as naw
+# import nlpaug.augmenter.word as naw
 import torch
 import torch.nn as nn
-
+from functools import partial
 
 pad_to_multiple_of = 16
 max_length = 512
@@ -19,12 +19,11 @@ class InfData(Dataset):
         text = self.df.text[idx]
         return text
 
-class TxtData(Dataset):
+class TxtData(InfData):
     def __init__(self, df, aug=None):
-        self.df = df
+        super().__init__(df)
         self.aug = aug
-    def __len__(self):
-        return self.df.shape[0]
+
     def __getitem__(self,idx):
         text = self.df.text[idx]
         if self.aug:
@@ -32,30 +31,34 @@ class TxtData(Dataset):
         label = self.df.label[idx]
         score = self.df.score[idx]
         return text, label, score
-    
-def collate_fn(data,tokenizer):
-    text, label, score = zip(*data)
-    text = tokenizer.batch_encode_plus(text,
-                                        pad_to_multiple_of=pad_to_multiple_of,
-                                        add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
-                                        max_length=max_length,  # Maximum length of the sequences
-                                        truncation=True,  # Truncate longer sequences to `max_length`
-                                        padding=True,  # Pad shorter sequences to `max_length`
-                                        return_attention_mask=True,  # Return attention masks
-                                        return_tensors='pt'  # Return PyTorch tensors)
-    )
-    input_ids,attention_mask = text['input_ids'], text['attention_mask']
-    return input_ids,attention_mask, torch.tensor(label, dtype=torch.float32), torch.tensor(score, dtype=torch.float32)
 
-def collate_inf(data,tokenizer):
-    text = data
-    text = tokenizer.batch_encode_plus(text,
-                                        pad_to_multiple_of=pad_to_multiple_of,
-                                        add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
-                                        max_length=max_length,  # Maximum length of the sequences
-                                        truncation=True,  # Truncate longer sequences to `max_length`
-                                        padding=True,  # Pad shorter sequences to `max_length`
-                                        return_attention_mask=True,  # Return attention masks
-                                        return_tensors='pt'  # Return PyTorch tensors)
-    )
-    return text['input_ids'], text['attention_mask']
+def collate_factory(data,tokenizer,IsTrain,prompt,prompt_mask):
+    # prompt should be encoded
+    if IsTrain:
+        text, label, score = zip(*data)
+    else:
+        text = data
+    kwargs = {  'add_special_tokens': True,  # Add '[CLS]' and '[SEP]'
+                'max_length': max_length,  # Maximum length of the sequences
+                'truncation': True,  # Truncate longer sequences to `max_length`
+                'return_attention_mask': True,  # Return attention masks
+                'return_tensors': 'pt'  # Return PyTorch tensors
+            }
+    if prompt is not None:
+        kwargs.update({'padding':'max_length','max_length': max_length-prompt.shape[1]})
+    else:
+        kwargs.update({'pad_to_multiple_of':pad_to_multiple_of,'padding':'longest'})
+    text = tokenizer.batch_encode_plus(text,**kwargs)
+    input_ids,attention_mask = text['input_ids'], text['attention_mask']
+    if prompt is not None:
+        n = input_ids.shape[0]
+        input_ids = torch.cat([input_ids,prompt.broadcast_to(n,-1)],1)
+        attention_mask = torch.cat([attention_mask,prompt_mask.broadcast_to(n,-1)],1)
+
+    if IsTrain:
+        return input_ids,attention_mask, torch.tensor(label, dtype=torch.float32), torch.tensor(score, dtype=torch.float32)
+    else:
+        return input_ids,attention_mask
+
+collate_fn = partial(collate_factory,IsTrain=True,prompt=None,prompt_mask=None)
+collate_inf = partial(collate_factory,IsTrain=False,prompt=None,prompt_mask=None)
