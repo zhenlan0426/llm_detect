c3f7df1d76721ccf59ca907c5af3007d993996a8
will <zhenlanwang0426@gmail.com>
2023-11-16 16:42:13 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,12 +1,46 @@
 from torch.utils.data import Dataset, DataLoader
 # import nlpaug.augmenter.word as naw
-from transformers import MistralForCausalLM, MistralForSequenceClassification
+# from transformers import MistralForCausalLM, MistralForSequenceClassification
 import torch
-import torch.nn as nn
+import numpy as np
 from functools import partial
+from peft import (
+    # get_peft_model,
+    # PeftType,
+    # TaskType,
+    PrefixTuningConfig,
+    PromptEncoderConfig,
+    PromptTuningConfig,
+    LoraConfig)
+import os
+import re
 
 pad_to_multiple_of = 16
 max_length = 512
+
+
+
+def get_next_folder_name(base_path):
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+        return os.path.join(base_path, "model01")
+
+    # List all items in the base directory
+    items = os.listdir(base_path)
+
+    # Filter out items that are not directories or don't match the pattern "modelXX"
+    dir_pattern = re.compile(r"model(\d+)")
+    model_dirs = [item for item in items if os.path.isdir(os.path.join(base_path, item)) and dir_pattern.match(item)]
+
+    if not model_dirs:
+        return os.path.join(base_path, "model01")
+
+    # Extract numbers and find the maximum
+    max_number = max(int(dir_pattern.match(d).group(1)) for d in model_dirs)
+
+    # Create the next folder name
+    next_folder_name = f"model{str(max_number + 1).zfill(2)}"
+    return os.path.join(base_path, next_folder_name)
 
 ############
 ### data ###
@@ -70,7 +104,8 @@ collate_inf = partial(collate_factory,IsTrain=False)
 #############
 
 class LM(object):
-    def setup(self, tokenizer,num_virtual_tokens,alpha,config_type):
+    def __init__(self, model,tokenizer,num_virtual_tokens,alpha,config_type):
+        self.model = model
         self.pos_ = tokenizer('Yes yes',add_special_tokens=False,return_attention_mask=False)['input_ids']
         self.neg_ = tokenizer('No no',add_special_tokens=False,return_attention_mask=False)['input_ids']
         self.vocab_size = tokenizer.vocab_size
@@ -80,14 +115,13 @@ class LM(object):
         self.loss_lm_ = torch.nn.CrossEntropyLoss()
         
     def predict(self,input_ids,attention_mask,*args,**kwargs):
-        if not hasattr(self,'alpha_'):
-            raise AttributeError('need to call setup first')
-        out = self.__call__(input_ids,attention_mask,*args,**kwargs)
-        logits = out.logits[:,-1,self.pos_].sum(-1) - out.logits[:,-1,self.neg_].sum(-1)
-        return logits
+        with torch.no_grad():
+            out = self.model(input_ids,attention_mask,*args,**kwargs)
+            logits = out.logits[:,-1,self.pos_].sum(-1) - out.logits[:,-1,self.neg_].sum(-1)
+            return logits
     
     def get_loss(self,input_ids,attention_mask,label,score,*args,**kwargs):
-        out = self.__call__(input_ids,attention_mask,*args,**kwargs)
+        out = self.model(input_ids,attention_mask,*args,**kwargs)
         loss_bce = self.loss_bce_(out.logits[:,-1,self.pos_].sum(-1) - out.logits[:,-1,self.neg_].sum(-1),label)
         
         # LM objective
@@ -102,23 +136,83 @@ class LM(object):
         return loss_bce + self.alpha_ * loss_lm
 
 class Classification(object):
-    def setup(self,alpha=0.05):
+    def __init__(self,model, alpha):
+        self.model = model
         self.alpha_ = alpha
         self.loss_bce_ = torch.nn.BCEWithLogitsLoss()
         self.loss_score_ = lambda y,yhat: torch.sum((y!=-1.)*torch.abs(yhat-y))/torch.sum((y!=-1.)+0.01)
         
     def predict(self,input_ids,attention_mask,*args,**kwargs):
-        if not hasattr(self,'alpha_'):
-            raise AttributeError('need to call setup first')
-        out = self.__call__(input_ids,attention_mask,*args,**kwargs).logits
-        return out[:,0]
+        with torch.no_grad():
+            out = self.model(input_ids,attention_mask,*args,**kwargs).logits
+            return out[:,0]
     
     def get_loss(self,input_ids,attention_mask,label,score,*args,**kwargs):
-        out = self.__call__(input_ids,attention_mask,*args,**kwargs).logits
+        out = self.model(input_ids,attention_mask,*args,**kwargs).logits
         return self.loss_bce_(out[:,0], label) + self.alpha_ * self.loss_score_(score,out[:,1])
 
-class MistralForClass(MistralForSequenceClassification,Classification):
-    pass
+def get_random_config(config_type,pred_type,TARGET_MODEL):
+    if config_type == 'prefix':
+        config_class = PrefixTuningConfig
+        config_kwargs = dict(task_type = 'CAUSAL_LM' if pred_type =='LM' else "SEQ_CLS",
+                            num_virtual_tokens = np.random.choice([16,24,32]), 
+                            encoder_hidden_size = np.random.choice([2048,4096,6144]), 
+                            prefix_projection = np.random.choice([True,False]),
+                            )
+        if TARGET_MODEL == "mistralai/Mistral-7B-v0.1":
+            config_kwargs.update({'token_dim':1024,'num_attention_heads':8})
+    elif config_type == 'prompt_encoder':
+        config_class = PromptEncoderConfig
+        config_kwargs = dict(task_type = 'CAUSAL_LM' if pred_type =='LM' else "SEQ_CLS", 
+                            num_virtual_tokens = np.random.choice([16,24,32]), 
+                            encoder_hidden_size = np.random.choice([2048,4096,6144]),
+                            encoder_dropout = np.random.rand()*0.25,
+                            encoder_num_layers = np.random.choice([1,2,3]),
+                            encoder_reparameterization_type = np.random.choice(['MLP','LSTM'])
+                            )
+    elif config_type == 'prompt_txt':
+        config_class = PromptTuningConfig
+        config_kwargs = dict(task_type = 'CAUSAL_LM' if pred_type =='LM' else "SEQ_CLS",
+                            prompt_tuning_init='TEXT', 
+                            num_virtual_tokens = np.random.choice([24,48,64]),
+                            prompt_tuning_init_text="Assessing the origin of an essay, whether it is a product of artificial intelligence, specifically a large language model (LLM), or the creative endeavor of a human student, involves a nuanced evaluation of various elements including the writing style, complexity of ideas, and the nuances in expression, a task that challenges us to discern if the piece reflects the unique perspective and contextual understanding characteristic of human thought, or if it embodies the sophisticated yet distinct patterns of language and content generation typical of advanced AI systems.",
+                            tokenizer_name_or_path=TARGET_MODEL)
+    elif config_type == 'LoRA':
+        config_class = LoraConfig
+        config_kwargs = dict(r=np.random.choice([32,64,128]),
+                            lora_alpha = 16,
+                            lora_dropout = np.random.rand()*0.25, 
+                            bias=np.random.choice(['none', 'all' , 'lora_only' ]),
+                            target_modules = ["q_proj","k_proj", "v_proj","o_proj"] if np.random.rand()<0.5 else ["q_proj","k_proj", "v_proj","o_proj","gate_proj", "up_proj", "down_proj" ]
+                            )
+    return config_class,config_kwargs
 
-class MistralForLM(MistralForCausalLM,LM):
-    pass
+config_map = {'prefix':PrefixTuningConfig,
+                'prompt_encoder':PromptEncoderConfig,
+                'prompt_txt': PromptTuningConfig,
+                'LoRA': LoraConfig,}
+
+def save_config(TARGET_MODEL, pred_type, config_type, epochs, alpha, aug_kwargs, config_kwargs):
+    config_dict = {
+        "TARGET_MODEL": TARGET_MODEL,
+        "pred_type": pred_type,
+        "config_type": config_type,
+        "epochs": epochs,
+        "alpha": alpha,
+        "aug_kwargs": aug_kwargs,
+        "config_class": config_type,
+        "config_kwargs": config_kwargs
+    }
+    return config_dict
+
+def load_config(config_dict):
+    TARGET_MODEL = config_dict["TARGET_MODEL"]
+    pred_type = config_dict["pred_type"]
+    config_type = config_dict["config_type"]
+    epochs = config_dict["epochs"]
+    alpha = config_dict["alpha"]
+    aug_kwargs = config_dict["aug_kwargs"]
+    config_class = config_map[config_dict["config_class"]]
+    config_kwargs = config_dict["config_kwargs"]
+
+    return TARGET_MODEL, pred_type, config_type, epochs, alpha, aug_kwargs, config_class, config_kwargs
